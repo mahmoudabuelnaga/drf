fcd863779c780893765c9b11cea7ea57d20e23d2
mahmoudabuelnaga <mahmoudaboelnaga392@gmail.com>
2022-03-29 02:31:09 +0200

    Using Function Based Views For Create Retrieve or List

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_create_api_view),
-    path('<int:pk>/', views.product_detail_api_view),
+    path('', views.product_alt_view),
+    path('<int:pk>/', views.product_alt_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import Product
 from .serializers import ProductSerializer
 # Create your views here.
@@ -20,6 +25,32 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+@api_view(['GET','POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    if request.method == 'GET':
+        # Detail
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+
+        # list
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+        
+    if request.method == 'POST':
+        # create an item
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content')
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid":"not good data"}, 400)
 
 
 
